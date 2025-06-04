986137bc0af236d9ac7c68c66f7242ba97ace0ab
Stuart Barlow <sbarlster@gmail.com>
2025-06-04 14:17:36 +0100

    I forgot to commit reflection - so here it is - and sync too

---
+++ sync/counter.go
@@ -0,0 +1,26 @@
+package sync
+
+import "sync"
+
+// type Counter interface {
+// 	Inc()
+// }
+
+type Counter struct {
+	mu    sync.Mutex
+	value int
+}
+
+func NewCounter() *Counter {
+	return &Counter{}
+}
+
+func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.value++
+}
+
+func (c *Counter) Value() int {
+	return c.value
+}
